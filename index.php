6dd5d3a9af3bdb02be71c7c786f06ab7a54098ee
Chanan Sukjirawanit <fort2002@hotmail.com>
2019-03-14 00:34:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,29 @@
 <?php
  $accessToken = "CwYcqVS3XK9U+VHvq16CWDtVIJZq/xqLdASXGTBuLZ1uUTq54Y8SsqBlY2H0dEjW8Jz+ldrd6yf+rTRJMlXIt2OcBTTlOwKJpnpOXyzyOirRdbFzm+/KrBsNcj32qPnFYgYvq12yD2Jl9d2CLrLUAwdB04t89/1O/w1cDnyilFU=";//copy ข้อความ Channel access token ตอนที่ตั้งค่า
-$content = file_get_contents('php://input');
+   $content = file_get_contents('php://input');
    $arrayJson = json_decode($content, true);
-$arrayHeader = array();
+   $arrayHeader = array();
    $arrayHeader[] = "Content-Type: application/json";
    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
-//รับข้อความจากผู้ใช้
+   //รับข้อความจากผู้ใช้
    $message = $arrayJson['events'][0]['message']['text'];
-
-#ตัวอย่าง Message Type "Text + Sticker"
-   if($message == "สวัสดี"){
-      $arrayPostData['to'] = "Cc314d9fa1df98d4aa56757d5dbece10d";
-      $arrayPostData['messages'][0]['type'] = "text";
-      $arrayPostData['messages'][0]['text'] = "test";
-      $arrayPostData['messages'][1]['type'] = "sticker";
-      $arrayPostData['messages'][1]['packageId'] = "2";
-      $arrayPostData['messages'][1]['stickerId'] = "34";
-      pushMsg($arrayHeader,$arrayPostData);
+   //รับ id ของผู้ใช้
+   #ตัวอย่าง Message Type "Text + Sticker"
+   $arrayPostData['to'] = "Cc314d9fa1df98d4aa56757d5dbece10d";
+   if($_POST['action'] == "submit" && isset($_POST['data'])){
+   	$result = explode(":", $_POST['data']);
+    $arrayPostData['messages'][0]['type'] = "text";
+    $arrayPostData['messages'][0]['text'] = "แจ้งเตือนยอดน้องปัจจุบัน \n - น้องทั้งหมด ".$result[0]." คน \n - น้องลาทั้งหมด ".$result[1]." คน \n - น้องคงเหลือ ".$result[2]." คน";
+   $arrayPostData['messages'][1]['type'] = "text";
+    $arrayPostData['messages'][1]['text'] = "ยอดน้องผู้ชายปัจจุบัน \n - น้องผู้ชายทั้งหมด ".$result[3]." คน \n - น้องผู้ชายลาทั้งหมด ".$result[4]." คน \n - น้องผู้ชายคงเหลือ ".$result[5]." คน";
+   $arrayPostData['messages'][2]['type'] = "text";
+    $arrayPostData['messages'][2]['text'] = "ยอดน้องผู้หญิงปัจจุบัน \n - น้องผู้หญิงทั้งหมด ".$result[6]." คน \n - น้องผู้หญิงลาทั้งหมด ".$result[7]." คน \n - น้องผู้หญิงคงเหลือ ".$result[8]." คน";
    }
-function pushMsg($arrayHeader,$arrayPostData){
-      $strUrl = "https://api.line.me/v2/bot/message/push";
-$ch = curl_init();
+   pushMsg($arrayHeader,$arrayPostData);
+  
+   function pushMsg($arrayHeader,$arrayPostData){
+      $strUrl = "https://api.line.me/v2/bot/message/multicast";
+      $ch = curl_init();
       curl_setopt($ch, CURLOPT_URL,$strUrl);
       curl_setopt($ch, CURLOPT_HEADER, false);
       curl_setopt($ch, CURLOPT_POST, true);
@@ -31,5 +34,5 @@ $ch = curl_init();
       $result = curl_exec($ch);
       curl_close ($ch);
    }
-exit;
+   exit;  
 ?>
