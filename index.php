edbc014fd3a0a3b6df999d18d502cb812f2c26e4
Chanan Sukjirawanit <fort2002@hotmail.com>
2019-03-16 17:29:36 +0700

    Update

--- index.php
+++ index.php
@@ -70,6 +70,8 @@
          $result = explode(":", $response);
          $arrayPostData['messages'][0]['type'] = "text";
          $arrayPostData['messages'][0]['text'] = "ยอดน้อง SEC 1 \n - ยอดน้องทั้งหมด ".$result[9]." คน \n - ยอดน้องลา ".$result[10]." คน \n - น้องคงเหลือ ".$result[11]." คน";
+         $arrayPostData['messages'][1]['type'] = "text";
+         $arrayPostData['messages'][1]['text'] = "ยอดน้องผู้ชายปัจจุบัน \n - น้องผู้ชายทั้งหมด ".$result[12]." คน \n - น้องผู้ชายลาทั้งหมด ".$result[13]." คน \n - น้องผู้ชายคงเหลือ ".$result[14]." คน";
          replyMsg($arrayHeader,$arrayPostData);
       }else{
          $id = $arrayJson['events'][0]['source']['userId'];
